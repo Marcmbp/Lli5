1d8c257bb3ef239e53d1c9c15205f0b26c0ea3c5
tolo <tcatala@iedib.net>
2024-03-11 09:37:28 +0100

    tolo

--- BaseConnectExample.java
+++ BaseConnectExample.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+// Tolo
 public class BaseConnectExample {
     public static void main(String[] args) {
         Connection conn = null;
