177866fe837ed8c77232e99b926bd7ee86c2b47b
tcatalaI <tcatala@iedib.net>
2024-03-20 11:26:15 +0100

    Tolo20

--- BaseConnectExample.java
+++ BaseConnectExample.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 // Tolo
+// Tolo 20/03/24
 public class BaseConnectExample {
     public static void main(String[] args) {
         Connection conn = null;
